cb962ed62d58191e91a0eedb41666500a62c22e0
Unphuc-Accelerater <kiran@unphuc.com>
2025-07-17 19:30:22 +0530

    Fix frameBorder attribute causing JSX parsing error

--- src/components/FeedbackForm/FeedbackFormContainer.jsx
+++ src/components/FeedbackForm/FeedbackFormContainer.jsx
@@ -189,7 +189,7 @@ Best regards`;
                   src={formUrl}
                   width="100%" 
                   height="100%" 
-                  frameBorder="0"
+                  style={{ border: 'none' }}
                   onLoad={() => setIframeLoaded(true)}
                 />
               </div>
